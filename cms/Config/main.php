c6069738139a1b67e6a44c883421de043767d684
Sergey <gizya_85@mail.ru>
2025-08-28 15:47:31 +0300

    Создание CMS на php - 28 урок (Language, View, Settings Update)

---
+++ cms/Config/main.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'base_url'        => 'http://cms.lock',
+    'defaultlang'     => 'english',
+    'defaultTimezone' => 'America/Chicago',
+    'defaultTheme'    => 'default'
+];
