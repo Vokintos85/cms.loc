aa9a1d67a60e877e699c15b6e7653e56a625745e
Sergey <gizya_85@mail.ru>
2025-09-06 17:30:30 +0300

    Создание CMS на php - 34 урок (Редактирование меню, Кастомизация админки)

--- engine/Config/service.php
+++ engine/Config/service.php
@@ -8,4 +8,6 @@ return [
     Engine\Service\Request\Provider::class,
     Engine\Service\Auth\Provider::class,
     Engine\Service\Load\Provider::class,
+    Engine\Service\Customize\Provider::class,
+
 ];
